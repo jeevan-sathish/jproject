914c53ba2d42cc5c8f443888f25c3d6eb17c3a15
jeevan-sathish <sathishjeevan2004@gmail.com>
2024-09-16 04:28:58 +0530

    Add files via upload

---
+++ NETFLIX CLONE PROJ/netflix.js
@@ -0,0 +1,51 @@
+// var x= document.getElementById("btn1");
+// x.style.backgroundColor="red";
+function showAlert(){
+    alert("You are on the home page!")
+}
+var x= document.getElementById("sub-btn");
+
+function submited(){
+    alert("thank you its processing......")
+}
+
+var y= document.getElementById("qbox");
+
+function show(){
+    y.style.display="hidden";
+}
+//open button functions
+function showMore(){
+    a=document.getElementById("sbox").style.display="block";
+}
+
+function showMore1(){
+    b=document.getElementById("sbox1").style.display="block";
+}
+
+function showMore2(){
+    c=document.getElementById("sbox2").style.display="block";
+}
+function showMore3(){
+    d=document.getElementById("sbox3").style.display="block";
+}
+function showMore4(){
+    e=document.getElementById("sbox4").style.display="block";
+}
+//close button funtions
+
+function closeClick0(){
+    document.getElementById("sbox").style.display="none"
+}
+function closeClick1(){
+    document.getElementById("sbox1").style.display="none"
+}
+function closeClick2(){
+    document.getElementById("sbox2").style.display="none"
+}
+function closeClick3(){
+    document.getElementById("sbox3").style.display="none"
+}
+function closeClick4(){
+    document.getElementById("sbox4").style.display="none"
+}
